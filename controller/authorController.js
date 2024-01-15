aac56ea1a8e3332076770dce754c33de916f2fd9
Pravin Patekar <125630602+PravinSimbaQuartz@users.noreply.github.com>
2024-01-15 13:06:42 +0530

    updated code

--- controller/authorController.js
+++ controller/authorController.js
@@ -1,6 +1,5 @@
 const authorModel = require("../model/authorModel")
-
-
+const { ObjectId } = require("mongoose").Types
 
 const createAuthor = async function (req, res) {
     try {
@@ -13,6 +12,7 @@ const createAuthor = async function (req, res) {
             isActive,
             gender
         } = req.body
+
 
         const authorData = await authorModel.create({
             firstName,
@@ -30,8 +30,110 @@ const createAuthor = async function (req, res) {
     } catch (error) {
         return res.status(500).send({ message: error.message })
     }
+}
+
+const findAllAuther = async (req, res) => {
+    try {
+
+        const allAuthor = await authorModel.find()
+
+        const authorCount = await authorModel.countDocuments()
+
+        res.status(200).send({ message: "Author data fetch successfully", count: authorCount, allAuthor })
+
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+    }
+}
+
+const findSingleAuthor = async (req, res) => {
+    try {
+        const { id } = req.params
+
+        const authorData = await authorModel.findOne({ _id: new ObjectId(id) })
+
+        if (!authorData) {
+            return res.status(404).send({ message: "Author not found" })
+
+        }
+
+        res.status(200).send({ message: "Author data fetch successfully", authorData })
+
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+
+    }
+
+}
+
+
+const updateAuthor = async (req, res) => {
+    try {
+        // const authorId = req.params.id
+        const { id } = req.params
+
+        const { firstName,
+            lastName,
+            email,
+            password,
+            mobileNumber,
+            isActive,
+            gender } = req.body
+
+
+        const updatedData = await authorModel.findOneAndUpdate(
+            { _id: new ObjectId(id) },
+            {
+                firstName,
+                lastName,
+                email,
+                password,
+                mobileNumber,
+                isActive,
+                gender
+            },
+            {
+                new: true
+            }
+        )
+
+        if (!updatedData) {
+            return res.status(404).send({ message: "Author not found" })
+        }
+        res.status(200).send({ message: "Author data updated successfully", updatedData })
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+
+    }
+
+}
+
+
+const deleteAuthor = async (req, res) => {
+    try {
+        const { id } = req.params
+
+        // const isExistAuthor = await authorModel.findOne({ _id: new ObjectId(id) })
+        // if (!isExistAuthor) {
+        //     return res.status(404).send({
+        //         message: "Author not found"
+        //     })
+        // }
+
+        const deletedAuthor = await authorModel.findOneAndDelete({ _id: new ObjectId(id) })
+        if (!deletedAuthor) {
+            return res.status(404).send({
+                message: "Author not found"
+            })
+        }
+        res.status(200).send({ message: "Author deleted successfully" })
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+
+    }
 
 
 }
 
-module.exports = { createAuthor }
+
+module.exports = { createAuthor, findAllAuther, findSingleAuthor, updateAuthor, deleteAuthor }
--- route/routes.js
+++ route/routes.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
-const { createAuthor } = require("../controller/authorController")
+const { createAuthor, findSingleAuthor, findAllAuther, updateAuthor, deleteAuthor } = require("../controller/authorController")
 
 // Autor route
 
 router.post("/author", createAuthor)
+router.get("/author", findAllAuther)
+router.get("/author/:id", findSingleAuthor)
+router.put("/author/:id", updateAuthor)
+router.delete("/author/:id", deleteAuthor)
 
 
 
